46b0265f53ac7dc360e9d95b1f4401ae6835f108
toert <pw177@mail.ru>
2017-06-09 21:36:27 +0300

    make vpiska

--- js/main_map.js
+++ js/main_map.js
@@ -1,14 +1,14 @@
     ymaps.ready(init);
       function init(){
         var map = new ymaps.Map("map", {
-                center: [55.740684, 37.618174],
+                center: [55.590346, 38.147420],
                 zoom: 15
             });
         map.behaviors.disable('scrollZoom');
-        placemark = new ymaps.Placemark([55.740684, 37.618174], {
-          hintContent: 'Твой Дом',
-          balloonContentHeader: 'Твой Дом',
-          balloonContentBody: 'Москва, Старомонетный переулок, 10'
+        placemark = new ymaps.Placemark([55.590346, 38.147420], {
+          hintContent: 'Хата Костика',
+          balloonContentHeader: 'Хата Костика',
+          balloonContentBody: 'пос. Кратово, ул. Симбирская, д.5, кв.4'
         });
         map.geoObjects.add(placemark);
       }
